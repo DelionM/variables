e817d745767594cfa50dca92cc0776365cf227b4
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 23:09:31 -0600

    Add files via upload

---
+++ variables/index.php
@@ -0,0 +1,22 @@
+<?php
+    $nombre = "Brayan Giovanni";
+    $Nombre = "Delion Martínez";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uso de Variables</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Mi nobre es $nombre ";
+            echo $Nombre;
+        ?>    
+    </h1>
+</body>
+</html>
